1106d4134c31e671e985f216a8dd7259abcb32a0
Gerrit Grunwald <han.solo@mac.com>
2024-05-09 07:32:01 +0200

    cosmetics

--- src/main/java/eu/hansolo/ControllerV1.java
+++ src/main/java/eu/hansolo/ControllerV1.java
@@ -42,10 +42,10 @@ public class ControllerV1 {
             if (file.getFilename().endsWith(".jar")) {
                 try(FileOutputStream fos = new FileOutputStream(file.getFilename())) {
                     fos.write(file.getBytes());
-                    File           jarfile  = new File(file.getFilename());
+                    File           jarfile  = new File("/app/" + file.getFilename());
                     TreeNode<Item> treeNode = Scanner.getClassesAndMethods(jarfile.getName());
                     String         jsonText = Helper.toJsonString(jarfile.getName(), treeNode, "", "", "");
-                    //jarfile.delete();
+                    jarfile.delete();
 
                     HttpResponse response = HttpResponse.ok(jsonText).contentType(MediaType.APPLICATION_JSON_TYPE).status(HttpStatus.OK);
                     return response;
